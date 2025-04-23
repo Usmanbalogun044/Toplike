2cad1182445815bb82198516c08ace0646954e5e
usman <usmanbalogun044@gmail.com>
2025-04-23 13:59:42 -0700

    lol
    h

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-   'allowed_origins' => explode(',', env('FRONTEND_URLS', 'http://localhost:3000')),
+    'allowed_origins' => [env('FRONTEND_URL', '*')],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'],
 
     'allowed_headers' => ['*'],
 
