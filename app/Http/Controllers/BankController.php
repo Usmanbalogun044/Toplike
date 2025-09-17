76d60ae5c6f4da19e70b69621048e050ee5a9be1
usman <usmanbalogun044@gmail.com>
2025-09-18 00:08:21 +0100

    new me

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -7,14 +7,15 @@ use Illuminate\Http\Request;
 class BankController extends Controller
 {
     public function getbankdetails(){
-        $user = auth()->user();
-         $bankAccount = $user->bankaccount()->first();
-        if (!$bankAccount) {
-            return response()->json(['message' => 'No bank account found'], 404);
-        }
-        return response()->json([
-            'message' => 'Bank account details retrieved successfully',
-            'bank_account' => $bankAccount,
-        ])->setStatusCode(200, 'Bank account details retrieved successfully');
+        // $user = auth()->user();
+        //  $bankAccount = $user->bankaccount()->first();
+        // if (!$bankAccount) {
+        //     return response()->json(['message' => 'No bank account found'], 404);
+        // }
+        // return response()->json([
+        //     'message' => 'Bank account details retrieved successfully',
+        //     'bank_account' => $bankAccount,
+        // ])->setStatusCode(200, 'Bank account details retrieved successfully');
+        return "bank details";
     }
 }
