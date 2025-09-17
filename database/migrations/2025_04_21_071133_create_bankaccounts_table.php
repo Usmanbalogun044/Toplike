a8c6ae3bfefe01e4bfa24ff11c63cd0985d38678
usman <usmanbalogun044@gmail.com>
2025-09-17 23:01:28 +0100

    new commit

--- database/migrations/2025_04_21_071133_create_bankaccounts_table.php
+++ database/migrations/2025_04_21_071133_create_bankaccounts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('bankaccounts', function (Blueprint $table) {
+    Schema::create('bank_accounts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string('bank_name');
